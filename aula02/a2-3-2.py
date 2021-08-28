f16d7517def0563bbe61eec7a45d521778f174a5
Wanderlei Cardoso Bittencourt <wlbittencourt@hotmail.com>
2021-08-28 18:20:57 -0300

    Tuplas

---
+++ aula02/a2-3-2.py
@@ -0,0 +1,9 @@
+# Curso: Python Fundamentos Proway
+# Aula: Dia 2 - Parte 3 - 2
+# Assunto: Tuplas 
+# Data: 2021-08-28
+
+#tuplas são criadas com parentes e nao podem mais ser alteradas
+bebidas = ('Coca-cola', 'Skol', 'Cachaça', 'Agua')
+print(bebidas[0]) # imprimindo primeiro elemento da tupla
+print(bebidas[2:]) # imprimindo parte da tupla
